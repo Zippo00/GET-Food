76b56ed81761a2ea522703ccb870cf9c1954b24b
Zippo00 <mikko_lempinen@outlook.com>
2025-03-25 18:32:48 +0200

    removed the functions

--- server/app.py
+++ server/app.py
@@ -8,6 +8,35 @@ from api.routes.orders_routes import orders_bp
 from api.routes.order_items_routes import order_items_bp
 from api.routes.order_status_routes import order_status_bp
 
+app = Flask(__name__)
+app.config.from_object(Config)
+    
+db.init_app(app)
+
+#blueprints
+app.register_blueprint(item_bp)
+app.register_blueprint(image_bp)
+app.register_blueprint(orders_bp)
+app.register_blueprint(order_items_bp)
+app.register_blueprint(order_status_bp)
+
+with app.app_context():
+        db.create_all()
+
+#Swagger initialization
+swagger = Swagger(app)
+
+@app.route("/")
+def home():
+    """
+    Welcome message
+    ---
+    responses:
+      200:
+        description: Welcome message
+    """
+    return {"message": "Welcome to the Food Ordering System API!"}, 200
+
 
 def init_app():
     """
@@ -58,6 +87,6 @@ def create_app():
 
     return app
 
-if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app = create_app()
+#     app.run(debug=True)
