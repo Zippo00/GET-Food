b31ed08d3f0f56d1fcf0e641c1e79fd4ea23268c
Zippo00 <mikko_lempinen@outlook.com>
2025-04-09 10:42:14 +0300

    minor tweak

--- server/app.py
+++ server/app.py
@@ -97,7 +97,7 @@ def create_app():
 
 if __name__ == "__main__":
 #     app = create_app()
-    app.run(host="0.0.0.0")
+    #app.run(host="0.0.0.0")
     uvicorn.run(app,
                 host="0.0.0.0",
                 port=8000
