20e26b8a4964fb7c5b757b348a74fd741ccbf662
Zippo00 <mikko_lempinen@outlook.com>
2025-04-09 12:05:33 +0300

    added debugging

--- server/app.py
+++ server/app.py
@@ -100,5 +100,6 @@ if __name__ == "__main__":
     #app.run(host="0.0.0.0")
     uvicorn.run(app,
                 host="0.0.0.0",
-                port=5000
+                port=5000,
+                debug=True
                 )
